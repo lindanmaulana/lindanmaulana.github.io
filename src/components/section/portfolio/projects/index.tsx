f2249ca1bb730b0b6f5777d216429694711a367e
Lindan Maulana <lindanstemga@gmail.com>
2024-12-22 11:36:12 +0700

    maintenance portfolio page

--- src/components/section/portfolio/projects/index.tsx
+++ src/components/section/portfolio/projects/index.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <LayoutSections styleSection="py-5">
      <ProjectFilter />
-      <div className="grid gap-4 lg:grid-cols-2">
+      <div className="grid gap-4 pb-10 lg:grid-cols-2">
         <ProjectListItems />
       </div>
     </LayoutSections>
